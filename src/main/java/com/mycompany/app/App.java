0e28748cd9a99e469345d3b2158e281b20e69b05
srikanth-suthari <suthari.srikanthchary@gmail.com>
2025-02-15 10:03:52 +0530

    Again modified App.java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,5 +6,6 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Maven!");
+	System.out.println("Maven is Working");
     }
 }
